8cba8c5194b2cadb99a77b0bcae078dbb1f0b7af
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2023-11-15 12:20:55 +0000

    Invoke event function

--- Assets/Player/Scripts/IsometricPlayerController.cs
+++ Assets/Player/Scripts/IsometricPlayerController.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class IsometricPlayerController : MonoBehaviour, Controls.IPlayerActions
 {
+    CharacterController controller;
 
+    private Vector2 direction;
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        controller.Move(direction * moveSpeed * Time.deltaTime);
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 readVector = context.ReadValue<Vector2>();
@@ -16,7 +29,9 @@ public class IsometricPlayerController : MonoBehaviour, Controls.IPlayerActions
 
     private Vector3 IsoVectorConvert(Vector3 vector)
     {
-        Quaternion rotation = Quaternion.Euler
+        Quaternion rotation = Quaternion.Euler(0, 45.0f, 0);
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(rotation);
+        Vector3 result = isoMatrix.MultiplyPoint3x4(vector);
+        return result;
     }
-    
 }
